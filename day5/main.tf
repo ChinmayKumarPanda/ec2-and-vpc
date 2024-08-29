e8304d97497770060af38f58de27c10268abb81a
chinu <chinmaypanda447@gmail.com>
2024-08-30 01:28:10 +0530

    updated

---
+++ day5/main.tf
@@ -0,0 +1,36 @@
+#-----------------------ec2 instance----------------------
+resource "aws_instance" "test" {
+  ami             = "ami-02b49a24cfb95941c"
+  instance_type   = "t2.micro"
+  availability_zone = "ap-south-1b"
+  key_name        = "projectkey"
+  
+  tags = {
+    Name = "dev_1"
+  }
+}
+#------------------------------s3 bucket---------------------------
+resource "aws_s3_bucket" "example" {
+  bucket = "chinudevops"
+
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+#------------------locking provision for statefile-----------------
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockId"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockId"
+    type = "S"
+  }
+
+  tags = {
+    Name = "TerraformStateLock"
+  }
+}
