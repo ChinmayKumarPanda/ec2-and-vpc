fd48386579f6e1ba90e0a935673578481c122af5
Chinmay Kumar Panda <126417086+ChinmayKumarPanda@users.noreply.github.com>
2024-09-25 13:20:41 +0530

    Update install.sh

--- project-terraform-devops-tools-install/install.sh
+++ project-terraform-devops-tools-install/install.sh
@@ -25,11 +25,11 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum -y install terraform
 
 #---------------------------------install tomcat------------------
-#sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-#sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
-#cd apache-tomcat-9.0.83
-#cd bin
-#chmod +x startup.sh
+sudo wget url https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
+sudo tar -xvzf apache-tomcat-9.0.83.tar.gz #untar
+cd apache-tomcat-9.0.83
+cd bin
+chmod +x startup.sh
 
 
 
